373229c121d2c2eb87981a45b8d952b4b075fde9
mathew-dennis <78794223+mathew-dennis@users.noreply.github.com>
2025-07-09 21:49:27 +0530

    test

--- src/cutieappthumbnail.cpp
+++ src/cutieappthumbnail.cpp
@@ -1,12 +1,26 @@
 #include "cutieappthumbnail.h"
 #include <QOpenGLTexture>
 #include <QOpenGLFunctions>
+#include <QOpenGLShaderProgram>
+#include <QOpenGLBuffer>
+#include <QOpenGLVertexArrayObject>
+#include <QDebug>
 
-// Internal FBO Renderer
-class AppThumbnailRenderer : public QQuickFramebufferObject::Renderer {
+// Renderer using modern OpenGL (GLSL shaders + VBO/VAO)
+class AppThumbnailRenderer : public QQuickFramebufferObject::Renderer,
+                             protected QOpenGLFunctions
+{
 public:
     AppThumbnailRenderer(CutieShell* shell, ForeignToplevelHandleV1* toplevel)
-        : m_shell(shell), m_toplevel(toplevel) {}
+        : m_shell(shell), m_toplevel(toplevel)
+    {
+        initializeOpenGL();
+    }
+
+    ~AppThumbnailRenderer() override {
+        if (m_program)
+            delete m_program;
+    }
 
     void render() override {
         if (!m_shell || !m_toplevel)
@@ -16,60 +30,101 @@ public:
         if (!texture)
             return;
 
-        texture->bind();
+        QOpenGLFunctions *f = QOpenGLContext::currentContext()->functions();
 
-        QOpenGLFunctions* f = QOpenGLContext::currentContext()->functions();
-        f->glEnable(GL_TEXTURE_2D);
-        f->glBegin(GL_QUADS);
+        f->glViewport(0, 0, m_viewportSize.width(), m_viewportSize.height());
+        f->glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+        f->glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        f->glTexCoord2f(0.0f, 1.0f); f->glVertex2f(-1.0f, -1.0f);
-        f->glTexCoord2f(1.0f, 1.0f); f->glVertex2f( 1.0f, -1.0f);
-        f->glTexCoord2f(1.0f, 0.0f); f->glVertex2f( 1.0f,  1.0f);
-        f->glTexCoord2f(0.0f, 0.0f); f->glVertex2f(-1.0f,  1.0f);
+        m_program->bind();
+        texture->bind(0);
+        m_program->setUniformValue("u_texture", 0);
 
-        f->glEnd();
+        m_vao.bind();
+        f->glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+        m_vao.release();
 
         texture->release();
+        m_program->release();
 
-        update(); // ensure continuous live update
+        update(); // schedule next render
+    }
+
+    QOpenGLFramebufferObject *createFramebufferObject(const QSize &size) override {
+        m_viewportSize = size;
+        QOpenGLFramebufferObjectFormat format;
+        format.setAttachment(QOpenGLFramebufferObject::CombinedDepthStencil);
+        format.setTextureTarget(GL_TEXTURE_2D);
+        format.setInternalTextureFormat(GL_RGBA);
+        return new QOpenGLFramebufferObject(size, format);
     }
 
 private:
+    void initializeOpenGL() {
+        initializeOpenGLFunctions(); // initialize QOpenGLFunctions
+
+        // Shader source code (OpenGL 3.3)
+        static const char *vertexShaderSrc =
+            R"(#version 330
+               in vec2 position;
+               in vec2 texCoord;
+               out vec2 v_texCoord;
+               void main() {
+                   v_texCoord = texCoord;
+                   gl_Position = vec4(position, 0.0, 1.0);
+               })";
+
+        static const char *fragmentShaderSrc =
+            R"(#version 330
+               uniform sampler2D u_texture;
+               in vec2 v_texCoord;
+               out vec4 fragColor;
+               void main() {
+                   fragColor = texture(u_texture, v_texCoord);
+               })";
+
+        // Compile shader
+        m_program = new QOpenGLShaderProgram();
+        if (!m_program->addShaderFromSourceCode(QOpenGLShader::Vertex, vertexShaderSrc))
+            qWarning() << "Vertex shader error:" << m_program->log();
+        if (!m_program->addShaderFromSourceCode(QOpenGLShader::Fragment, fragmentShaderSrc))
+            qWarning() << "Fragment shader error:" << m_program->log();
+        if (!m_program->link())
+            qWarning() << "Shader link error:" << m_program->log();
+
+        // Vertex data: 4 vertices (x, y, u, v)
+        GLfloat vertices[] = {
+            -1.0f, -1.0f,  0.0f, 1.0f,
+             1.0f, -1.0f,  1.0f, 1.0f,
+            -1.0f,  1.0f,  0.0f, 0.0f,
+             1.0f,  1.0f,  1.0f, 0.0f
+        };
+
+        m_vbo.create();
+        m_vbo.bind();
+        m_vbo.allocate(vertices, sizeof(vertices));
+
+        m_vao.create();
+        m_vao.bind();
+
+        int posLoc = m_program->attributeLocation("position");
+        int texLoc = m_program->attributeLocation("texCoord");
+
+        m_program->enableAttributeArray(posLoc);
+        m_program->setAttributeBuffer(posLoc, GL_FLOAT, 0, 2, 4 * sizeof(GLfloat));
+
+        m_program->enableAttributeArray(texLoc);
+        m_program->setAttributeBuffer(texLoc, GL_FLOAT, 2 * sizeof(GLfloat), 2, 4 * sizeof(GLfloat));
+
+        m_vao.release();
+        m_vbo.release();
+    }
+
     CutieShell* m_shell = nullptr;
     ForeignToplevelHandleV1* m_toplevel = nullptr;
+
+    QOpenGLShaderProgram* m_program = nullptr;
+    QOpenGLVertexArrayObject m_vao;
+    QOpenGLBuffer m_vbo{QOpenGLBuffer::VertexBuffer};
+    QSize m_viewportSize;
 };
-
-// --------- CutieAppThumbnail implementation ----------
-
-CutieAppThumbnail::CutieAppThumbnail(QQuickItem *parent)
-    : QQuickFramebufferObject(parent)
-{
-}
-
-QQuickFramebufferObject::Renderer* CutieAppThumbnail::createRenderer() const {
-    return new AppThumbnailRenderer(m_wlc, m_toplevel);
-}
-
-CutieShell* CutieAppThumbnail::wlc() {
-    return m_wlc;
-}
-
-void CutieAppThumbnail::setWlc(CutieShell* wlc) {
-    if (m_wlc == wlc)
-        return;
-    m_wlc = wlc;
-    emit wlcChanged(m_wlc);
-    update(); // ensure redraw
-}
-
-ForeignToplevelHandleV1* CutieAppThumbnail::toplevel() {
-    return m_toplevel;
-}
-
-void CutieAppThumbnail::setToplevel(ForeignToplevelHandleV1* toplevel) {
-    if (m_toplevel == toplevel)
-        return;
-    m_toplevel = toplevel;
-    emit toplevelChanged(m_toplevel);
-    update(); // ensure redraw
-}
