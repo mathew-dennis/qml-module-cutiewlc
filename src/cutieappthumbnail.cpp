91ed01020b0a7b307b8ca57e83a2ab1ccd42a4a2
mathew-dennis <78794223+mathew-dennis@users.noreply.github.com>
2025-10-26 16:46:42 +0530

    test

--- src/cutieappthumbnail.cpp
+++ src/cutieappthumbnail.cpp
@@ -1,76 +1,110 @@
 #include "cutieappthumbnail.h"
+#include <QTimer>
+#include <QDebug>
 
 CutieAppThumbnail::CutieAppThumbnail(QQuickItem *parent)
-	: QQuickPaintedItem(parent)
+    : QQuickPaintedItem(parent)
 {
 }
 
 CutieShell *CutieAppThumbnail::wlc()
 {
-	return m_wlc;
+    return m_wlc;
 }
 
 void CutieAppThumbnail::setWlc(CutieShell *wlc)
 {
-	if (m_wlc == wlc)
-		return;
-	m_wlc = wlc;
-	connect(m_wlc, &CutieShell::thumbnailDamage, this,
-		&CutieAppThumbnail::onThumbnailDamage);
-	emit wlcChanged(m_wlc);
+    if (m_wlc == wlc)
+        return;
+    m_wlc = wlc;
+    connect(m_wlc, &CutieShell::thumbnailDamage, this,
+            &CutieAppThumbnail::onThumbnailDamage);
+    emit wlcChanged(m_wlc);
 }
 
 ForeignToplevelHandleV1 *CutieAppThumbnail::toplevel()
 {
-	return m_toplevel;
+    return m_toplevel;
 }
 
 void CutieAppThumbnail::setToplevel(ForeignToplevelHandleV1 *toplevel)
 {
-	if (m_toplevel == toplevel)
-		return;
-	m_toplevel = toplevel;
-	emit toplevelChanged(m_toplevel);
-	if (m_wlc && m_toplevel && !copying) {
-		copying = true;
-		QTimer::singleShot(50, this, &CutieAppThumbnail::newFrame);
-	}
+    if (m_toplevel == toplevel)
+        return;
+    m_toplevel = toplevel;
+    emit toplevelChanged(m_toplevel);
+
+    if (m_wlc && m_toplevel && !copying && !m_frozen) {
+        copying = true;
+        QTimer::singleShot(50, this, &CutieAppThumbnail::newFrame);
+    }
 }
 
 void CutieAppThumbnail::paint(QPainter *painter)
 {
-	painter->drawImage(QRectF(QPointF(), size()), m_image);
+    painter->drawImage(QRectF(QPointF(), size()), m_image);
 }
 
 void CutieAppThumbnail::newFrame()
 {
-	m_frame = m_wlc->getThumbnail(m_toplevel);
-	connect(m_frame, &ScreencopyFrameV1::ready, this,
-		&CutieAppThumbnail::onReady);
-	connect(m_frame, &ScreencopyFrameV1::failed, this,
-		&CutieAppThumbnail::onFailed);
+    if (!m_wlc || !m_toplevel || m_frozen)
+        return;
+
+    m_frame = m_wlc->getThumbnail(m_toplevel);
+    connect(m_frame, &ScreencopyFrameV1::ready, this,
+            &CutieAppThumbnail::onReady);
+    connect(m_frame, &ScreencopyFrameV1::failed, this,
+            &CutieAppThumbnail::onFailed);
 }
 
 void CutieAppThumbnail::onReady(QImage image)
 {
-	m_image = image;
-	update(QRect(QPoint(), textureSize()));
-	copying = false;
+    if (m_frozen) {
+        copying = false;
+        return;
+    }
+
+    m_image = image;
+    update(QRect(QPoint(), textureSize()));
+    copying = false;
 }
 
 void CutieAppThumbnail::onFailed()
 {
-	copying = false;
+    copying = false;
 }
 
 void CutieAppThumbnail::onThumbnailDamage(void *object)
 {
-	if (!m_toplevel)
-		return;
-	ForeignToplevelHandleV1 *handle =
-		m_toplevel->manager()->handleFromObject(object);
-	if (handle == m_toplevel && !copying) {
-		copying = true;
-		QTimer::singleShot(50, this, &CutieAppThumbnail::newFrame);
-	}
+    if (!m_toplevel || m_frozen)
+        return;
+
+    ForeignToplevelHandleV1 *handle =
+        m_toplevel->manager()->handleFromObject(object);
+    if (handle == m_toplevel && !copying) {
+        copying = true;
+        QTimer::singleShot(50, this, &CutieAppThumbnail::newFrame);
+    }
+}
+
+// --- Freeze / Resume ---
+void CutieAppThumbnail::freeze()
+{
+	if (m_frozen)
+        return;
+
+    m_frozen = true;
+    qDebug() << "module - CutieAppThumbnail - frozen";
+}
+
+void CutieAppThumbnail::resume()
+{
+    if (!m_frozen)
+        return;
+
+    m_frozen = false;
+	qDebug() << "module - CutieAppThumbnail - resumed";
+
+    if (m_toplevel && !copying)
+        QTimer::singleShot(50, this, &CutieAppThumbnail::newFrame);
 }
--- src/cutieappthumbnail.h
+++ src/cutieappthumbnail.h
@@ -8,36 +8,40 @@
 #include <qt6-screencopy.h>
 
 class CutieAppThumbnail : public QQuickPaintedItem {
-	Q_OBJECT
-	QML_ELEMENT
-	Q_PROPERTY(CutieShell *wlc READ wlc WRITE setWlc NOTIFY wlcChanged)
+    Q_OBJECT
+    QML_ELEMENT
+    Q_PROPERTY(CutieShell *wlc READ wlc WRITE setWlc NOTIFY wlcChanged)
 	Q_PROPERTY(ForeignToplevelHandleV1 *toplevel READ toplevel WRITE
 			   setToplevel NOTIFY toplevelChanged)
-    public:
-	CutieAppThumbnail(QQuickItem *parent = nullptr);
-	void paint(QPainter *painter) override;
+public:
+    CutieAppThumbnail(QQuickItem *parent = nullptr);
+    void paint(QPainter *painter) override;
 
-	CutieShell *wlc();
-	void setWlc(CutieShell *wlc);
+    CutieShell *wlc();
+    void setWlc(CutieShell *wlc);
 
-	ForeignToplevelHandleV1 *toplevel();
-	void setToplevel(ForeignToplevelHandleV1 *toplevel);
+    ForeignToplevelHandleV1 *toplevel();
+    void setToplevel(ForeignToplevelHandleV1 *toplevel);
 
-    signals:
-	void wlcChanged(CutieShell *wlc);
-	void toplevelChanged(ForeignToplevelHandleV1 *toplevel);
+    // Freeze / Resume
+    Q_INVOKABLE void freeze();
+    Q_INVOKABLE void resume();
 
-    private slots:
-	void onReady(QImage image);
-	void onFailed();
-	void onThumbnailDamage(void *object);
+signals:
+    void wlcChanged(CutieShell *wlc);
+    void toplevelChanged(ForeignToplevelHandleV1 *toplevel);
 
-	void newFrame();
+private slots:
+    void onReady(QImage image);
+    void onFailed();
+    void onThumbnailDamage(void *object);
+    void newFrame();
 
-    private:
-	CutieShell *m_wlc = nullptr;
-	ForeignToplevelHandleV1 *m_toplevel = nullptr;
-	ScreencopyFrameV1 *m_frame = nullptr;
-	QImage m_image;
-	bool copying = false;
+private:
+    CutieShell *m_wlc = nullptr;
+    ForeignToplevelHandleV1 *m_toplevel = nullptr;
+    ScreencopyFrameV1 *m_frame = nullptr;
+    QImage m_image;
+    bool copying = false;
+    bool m_frozen = false;   // freeze/resume flag
 };
